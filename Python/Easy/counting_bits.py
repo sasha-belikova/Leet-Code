110a86d9d2d7783556363204cbb09243f7e8b344
Aleksandra Belikova <belikovaleksandra@gmail.com>
2026-09-23 22:29:43 +0200

    Fixed counting bits (python version

--- Python/Easy/counting_bits.py
+++ Python/Easy/counting_bits.py
@@ -6,8 +6,5 @@ def count(n):
             rest = i % 2      # converting a number to the binary system without bin
             t.append(rest)    # by saving the remainder of division by two
             i //= 2
-        if sum(t) > 1:
-            list.append(sum(t))
-        else:
-            list.append(sum(t))
+        list.append(sum(t))
     return list
